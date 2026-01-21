53a18cc7d1a4f446438ffdf457037084ef9456ac
muntasir010 <naeemtasir03@gmail.com>
2026-01-21 21:02:41 +0600

    server side setup successfully

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+  })
+  .catch(err => console.error(err));
